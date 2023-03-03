62b8706e33d6b06746b3cf17369c951617345e74
Riko <xxy1836@gmail.com>
2023-03-03 21:57:07 +0800

    add brute

--- src/brute/wordlists/de_duplication.py
+++ src/brute/wordlists/de_duplication.py
@@ -1,10 +1,13 @@
+#!/bin/env python3
 
 str_list = []
 with open('all.txt', 'r') as fp:
     for i in fp.readlines():
         i = i.strip()
-        if str(i) > 0 and i not in str_list:
+        if len(i) > 0 and i not in str_list:
             str_list.append(i)
 
 with open('de_all.txt', 'w+') as fp:
-    fp.writelines(str_list)
+    for s in str_list:
+        fp.writelines(s)
+        fp.writelines('\n')
