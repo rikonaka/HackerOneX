80f3cbf1337a161ecfcf7586b49b81f817c9cced
Riko <xxy1836@gmail.com>
2023-05-31 15:45:18 +0800

    try to develop in root

--- src/main.rs
+++ src/main.rs
@@ -431,6 +431,66 @@ fn honeypot(p: &mut Parameters) {
     commands.run(p);
 }
 
+#[test]
+fn test_raw_socket() {
+    use pnet::datalink::Channel::Ethernet;
+    use pnet::datalink::{self, NetworkInterface};
+    use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
+    use pnet::packet::{MutablePacket, Packet};
+
+    let interface_name = String::from("eno1");
+    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
+    println!("{}", interface_name);
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    println!("{:?}", interfaces);
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap();
+
+    // Create a new channel, dealing with layer 2 packets
+    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}",
+            e
+        ),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let packet = EthernetPacket::new(packet).unwrap();
+
+                // Constructs a single packet, the same length as the the one received,
+                // using the provided closure. This allows the packet to be constructed
+                // directly in the write buffer, without copying. If copying is not a
+                // problem, you could also use send_to.
+                //
+                // The packet is sent once the closure has finished executing.
+                tx.build_and_send(1, packet.packet().len(), &mut |mut new_packet| {
+                    let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
+
+                    // Create a clone of the original packet
+                    new_packet.clone_from(&packet);
+
+                    // Switch the source and destination
+                    new_packet.set_source(packet.get_destination());
+                    new_packet.set_destination(packet.get_source());
+                });
+            }
+            Err(e) => {
+                // If an error occurs, we can handle it here
+                panic!("An error occurred while reading: {}", e);
+            }
+        }
+    }
+}
+
 fn main() {
     // run backend first
     let args = Args::parse();
